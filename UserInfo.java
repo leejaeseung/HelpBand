b0c9fa26862f21d0e07b73e1d882afccf8c60785
leejaeseung <wasd222@naver.com>
2019-12-20 20:06:42 +0900

    클래스들만 추가해봄

--- UserInfo.java
+++ UserInfo.java
@@ -1,2 +1,27 @@
 public class UserInfo {
+    private String name;
+    private int age;
+    private int personalNum;
+    private String[] ProtectorList;
+
+    public UserInfo(String name, int age, int personalNum, String ProtectorID){
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getPersonalNum() {
+        return personalNum;
+    }
+
+    public String[] getProtectorList() {
+        return ProtectorList;
+    }
+
+
 }

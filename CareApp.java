f026ed737e310e2ec00376e5e26f9b2971b345fa
leejaeseung <wasd222@naver.com>
2019-12-21 18:40:43 +0900

    GUI도 넣어 보았어용~♡

--- CareApp.java
+++ CareApp.java
@@ -2,15 +2,9 @@ public class CareApp {
 
     private String usercode;
     private UserInfo UI;
-    private String BandName;
     private Device myDevice;
     private Bands myBand;
 
-    public CareApp(String name, int age, int personalNum, Bands myBand){
-        UI = new UserInfo(name, age, personalNum);
-        this.myBand = myBand;
-        this.myDevice = new Adapter();
-    }
     public void StartBioAnalyze() throws InterruptedException{
         JudgeAI_IF JA = new JudgeAI();
         UI.addProtector(new WatchingApp(1));
@@ -23,7 +17,9 @@ public class CareApp {
             Thread.sleep(1000);
         }
     }
-    public void enrolUser(){
-
+    public void enrolUser(String name, int age, int personalNum, Bands myBand){
+        UI = new UserInfo(name, age, personalNum);
+        this.myBand = myBand;
+        this.myDevice = new Adapter();
     }
 }
--- WatchingApp.java
+++ WatchingApp.java
@@ -1,27 +1,77 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class WatchingApp implements Observer{
 
     private int usercode;
     private UserStatus myUS;
-    private int mynum;
+    private JFrame JF;
+    private JLabel TemperLabel;
+    private JLabel PulseLabel;
+    private JLabel SymptomLabel;
+    private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 
     public WatchingApp(int num){
-        mynum = num;
-    }
 
+        JF = new JFrame();
+        JF.setTitle("보호자" + num);
+        JF.setSize(250, 250);
+        //JF.setLocationRelativeTo(null);
+        JF.setLocation(250*(num - 1), (dim.height/2)-(JF.getHeight()/2));
+        JF.setResizable(false);
+        JF.setLayout(null);
+        JF.setVisible(true);
+        JF.setDefaultCloseOperation(JF.EXIT_ON_CLOSE);
+
+        TemperLabel = new JLabel();
+        TemperLabel.setOpaque(true);
+        TemperLabel.setBackground(new Color(0,0,0,0));
+        TemperLabel.setBounds(35,55,300,50);
+        TemperLabel.setForeground(Color.black);
+
+        PulseLabel = new JLabel();
+        PulseLabel.setOpaque(true);
+        PulseLabel.setBackground(new Color(0,0,0,0));
+        PulseLabel.setBounds(35,75,300,50);
+        PulseLabel.setForeground(Color.black);
+
+        SymptomLabel = new JLabel();
+        SymptomLabel.setOpaque(true);
+        SymptomLabel.setBackground(new Color(0,0,0,0));
+        SymptomLabel.setBounds(35,95,300,50);
+        SymptomLabel.setForeground(Color.black);
+
+        JF.add(TemperLabel);
+        JF.add(PulseLabel);
+        JF.add(SymptomLabel);
+    }
     public void enrolProtector(){
 
     }
     public void display(){
-        System.out.println("보호자 " + mynum);
-        System.out.println("체온 : " + myUS.getTemperature());
-        System.out.println("맥박 : " + myUS.getPulse());
-        System.out.println("증상 : " + myUS.getSymptom());
+
+        TemperLabel.setText("체온 : " + myUS.getTemperature());
+        PulseLabel.setText("맥박 : " + myUS.getPulse());
+        SymptomLabel.setText("증상 : " + myUS.getSymptom());
+        JF.repaint();
+
+        //System.out.println("보호자 " + mynum);
+        //System.out.println("체온 : " + myUS.getTemperature());
+        //System.out.println("맥박 : " + myUS.getPulse());
+        //System.out.println("증상 : " + myUS.getSymptom());
     }
     public void updateUserStatus(UserStatus US){
         myUS = US;
         display();
     }
     public void alert(){
-        System.out.println("비상!비상!비상!!!!!!!!");
+        JFrame AlertFrame = new JFrame();
+        AlertFrame.setTitle("위급 상황");
+        AlertFrame.setSize(250, 250);
+        //AlertFrame.setLocationRelativeTo();
+        AlertFrame.setResizable(false);
+        AlertFrame.setLayout(null);
+        AlertFrame.setVisible(true);
+        //System.out.println("비상!비상!비상!!!!!!!!");
     }
 }

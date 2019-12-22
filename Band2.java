80b4abe63fa051e4cb60f3aa2d0ca7ee0b861b47
leejaeseung <wasd222@naver.com>
2019-12-22 13:53:36 +0900

    .

--- Band2.java
+++ Band2.java
@@ -4,10 +4,10 @@ public class Band2 implements Bands{
 
     public int getTemperature(){
         Random rd = new Random();
-        return rd.nextInt(100) + 300;
+        return rd.nextInt(3) + 36;
     };
     public int getPulse(){
         Random rd = new Random();
-        return rd.nextInt(100) + 100;
+        return rd.nextInt(100) + 40;
     };
 }
